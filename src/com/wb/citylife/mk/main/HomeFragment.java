c2efe4b3702c21eabdd506a4725d7e45c7cfffc4
Administrator <Administrator@lbx-PC>
2014-04-23 23:24:38 +0800

    框架修改

---
+++ src/com/wb/citylife/mk/main/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.wb.citylife.mk.main;
+
+import com.wb.citylife.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment{
+	
+	private Activity mActivity;
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		mActivity = activity;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.main_item_home, container, false);		
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);		
+		initView(view);
+	}
+	
+	private void initView(View view) {
+		
+	}
+}
+	
